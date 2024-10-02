7bcd6caf88b4f545d8f5608a6588335de517d52f
Дмитрий <3deat10poll@gmail.com>
2024-10-02 10:56:53 +0300

    s7: реализован метод выгрузки всех видов задач из файла

--- src/manager/FileBackedTaskManager.java
+++ src/manager/FileBackedTaskManager.java
@@ -1,39 +1,35 @@
 package manager;
 
+import enumeration.Status;
+import enumeration.TypeTask;
 import exception.ManagerSaveException;
 import tasks.Epic;
 import tasks.SubTask;
 import tasks.Task;
 
 import java.io.*;
-import java.nio.file.Path;
-import java.util.ArrayList;
 
 
-public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager  {
+
+public class FileBackedTaskManager extends InMemoryTaskManager {
 
     private final File file;
 
 
-    public FileBackedTaskManager( File file) {
+    public FileBackedTaskManager(File file) {
         super();
         this.file = file;
     }
 
-    public void saveTas(Task task){
-        saveTask(task);
-        save();
-    }
-//----------------------------------------------------------------------------------------------------------------------
 
+//----------------------------------------------------------------------------------------------------------------------
 
 
     // метод для сохранения всех видов задач в файл;
 
-    public void save(){
-        sortList(getTasks());
-        /*
-        try (Writer writer = new FileWriter(file);){
+    public void save() {
+
+        try (Writer writer = new FileWriter(file)) {
 
             writer.write("id,type,name,status,description,epic\n");
             for (Integer key : taskMap.keySet()) {
@@ -47,20 +43,127 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
             }
         } catch (IOException exp) {
             throw new ManagerSaveException("Произошла ошибка записи в файл", exp);
-        }*/
-    }
-
-    public ArrayList<String> sortList(ArrayList<Task> list){
-        ArrayList<String> sortFile = new ArrayList<>();
-
-        for(Task text : list){
-            System.out.println(text);
-            sortFile.add(text.toString());
         }
-
-
-        return sortFile;
     }
 
+    public FileBackedTaskManager downloadingFromAFile(File file) {
+        FileBackedTaskManager manager = new FileBackedTaskManager(file);
+        TaskManager taskManager = Managers.getDefault();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            boolean isFirstLine = true;
+            while ((line = br.readLine()) != null) {
+                if (isFirstLine) {
+                    isFirstLine = false;
+                    continue;
+                }
+                String[] parts = line.split(",");
+                int id = Integer.parseInt(parts[0]);
+                String type = parts[1];
+                String name = parts[2];
+                String status = parts[3];
+                String description;
+                int idOfEpic = 0;
+                if (id > super.idUp) {
+                    super.idUp = id;
+                }
+                if (parts.length > 4) {
+                    description = parts[4];
+                } else {
+                    description = "";
+                }
+                if (type.equals("SUBTASK")) {
+                    idOfEpic = Integer.parseInt(parts[5]);
+                }
+                switch (TypeTask.valueOf(type)) {
+                    case SUBTASK -> {
+                        SubTask subtask = new SubTask(idOfEpic, name, description, id, Status.valueOf(status));
+                        super.saveSubTask( subtask);
+
+                    }
+                    case TASK -> {
+                        Task task = new Task(name, description, id, Status.valueOf(status));
+                        super.saveTask(task);
+                    }
+                    case EPIC -> {
+                        Epic epic = new Epic(name, description, id, Status.valueOf(status));
+                        super.saveEpic(epic);
+                    }
+                }
+            }
+            return manager;
+        } catch (IOException e) {
+            throw new ManagerSaveException("Saving error!");
+        }
+    }
+
+    // -----------------------------------------------------------------------------------------------------------
+
+
+    @Override
+    public void saveTask(Task savetheTask) {
+        super.saveTask(savetheTask);
+        save();
+    }
+
+    @Override
+    public void saveEpic(Epic savetheEpic) {
+        super.saveEpic(savetheEpic);
+        save();
+    }
+
+    @Override
+    public void saveSubTask(SubTask saveSubTask) {
+        super.saveSubTask(saveSubTask);
+        save();
+    }
+    @Override
+    public void clearContent() {
+        super.clearContent();
+        save();
+    }
+
+    @Override
+    public void clearTasks() {
+        super.clearTasks();
+        save();
+    }
+
+    @Override
+    public void clearSubTasksOfEpic(int epicId) {
+        super.clearSubTasksOfEpic(epicId);
+        save();
+    }
+
+    @Override
+    public void clearEpics() {
+        super.clearEpics();
+        save();
+    }
+
+    @Override
+    public void clearSubtasks() {
+        super.clearSubtasks();
+        save();
+    }
+
+    @Override
+    public void deleteTaskId(int numberId) {
+        super.deleteTaskId(numberId);
+        save();
+    }
+
+    @Override
+    public void deleteSubTaskId(int numberId) {
+        super.deleteSubTaskId(numberId);
+        save();
+    }
+
+    @Override
+    public void deleteEpicId(int numberId) {
+        super.deleteEpicId(numberId);
+        save();
+    }
 
 }
--- tests/manager/FileBackedTaskManagerTest.java
+++ tests/manager/FileBackedTaskManagerTest.java
@@ -4,6 +4,7 @@ import enumeration.Status;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tasks.Epic;
+import tasks.SubTask;
 import tasks.Task;
 
 import java.io.File;
@@ -12,17 +13,45 @@ import java.io.IOException;
 public class FileBackedTaskManagerTest {
     File temp = new File("Task.csv");
 
+    TaskManager taskManager = Managers.getDefault();
+    FileBackedTaskManager manager = new FileBackedTaskManager(temp);
+
 
     @Test
     public void saveTask() throws IOException {
 
-        FileBackedTaskManager manager = new FileBackedTaskManager(temp);
+
         Task task1 = new Task("Task1", "Description1",1,Status.NEW);
         Task task2 = new Task("Task2", "Description2",2,Status.DONE);
        // Epic epic1 = new Epic("Epic1", "Description1");
-        manager.saveTas(task1);
-        manager.saveTas(task2);
+        manager.saveTask(task1);
+        manager.saveTask(task2);
       //  manager.add(epic1);
+    }
+
+    @Test
+    public void saveEpic(){
+        Epic epic3 = new Epic("Test titleEpic", "Test description", taskManager.getIdUp(), Status.NEW);
+        SubTask sub5 = new SubTask(epic3.id, "Test titleSub1", "Test in Epic", taskManager.getIdUp(), Status.NEW);
+        SubTask sub6 = new SubTask(epic3.id, "Test titleSub1", "Test in Epic", taskManager.getIdUp(), Status.NEW);
+
+        manager.saveEpic(epic3);
+        manager.saveSubTask(sub5);
+        manager.saveSubTask(sub6);
+
+        taskManager.clearContent();// удаляем все эпики с подзадачами через InMemoryTaskManager для того чтобы не сохранилась новая версия файла
+
+        for (Epic t : taskManager.getEpics()){
+            System.out.println(t);
+        }
+
+
+
+        manager.downloadingFromAFile(temp);
+
+        for (Epic t : taskManager.getEpics()){
+            System.out.println(t.toString());
+        }
     }
 
 
