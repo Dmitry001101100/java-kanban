4fc75f9c790fd8ddae3d69d087c2eb2cb643fcf6
Дмитрий.Щ <deat10poll@yandex.ru>
2025-02-20 11:39:53 +0300

    s9: переделал метод обработки эндпоинтов

--- src/HttpTaskServer/handle/BaseHandle.java
+++ src/HttpTaskServer/handle/BaseHandle.java
@@ -30,7 +30,6 @@ public class BaseHandle {
         String path = exchange.getRequestURI().getPath();
         String[] pathParts = path.split("/");
 
-
         try {
             String idStr = pathParts[3];
             return Optional.of(Integer.parseInt(idStr));
@@ -54,58 +53,75 @@ public class BaseHandle {
     protected Endpoint getEndpoint(String path, String requestMethod) { // создание эндпоинта
         String[] pathParts = path.split("/");
 
-        if (requestMethod.equals("GET")) {
-            if (pathParts[2].equals("tasks")) {
-                if (pathParts.length == 3) {
-                    return Endpoint.GET_TASKS;
-                } else {
-                    return Endpoint.GET_TASK;
-                }
-            } else if (pathParts[2].equals("subtasks")) {
-                if (pathParts.length == 3) {
-                    return Endpoint.GET_SUBTASKS;
-                } else {
-                    return Endpoint.GET_SUBTASK;
-                }
-            } else if (pathParts[2].equals("epics")) {
-                if (pathParts.length == 5) {
-                    return Endpoint.GET_SUBTASK_BY_EPIC; // длина 5
-                } else if (pathParts.length == 3) {
-                    return Endpoint.GET_EPICS; // длинна 3
-                } else if (pathParts.length == 4) {
-                    return Endpoint.GET_EPIC; // длинна 4
-                }
-            } else if (pathParts[2].equals("history")) {
-                return Endpoint.GET_HISTORY;
-            } else if (pathParts[2].equals("prioritized")) {
-                return Endpoint.GET_PRIORITIZED;
-            }
-        } else if (requestMethod.equals("POST")) {
-            if (pathParts[2].equals("tasks")) {
-                return Endpoint.POST_TASK;
-            } else if (pathParts[2].equals("subtasks")) {
-                return Endpoint.POST_SUBTASK;
-            } else if (pathParts[2].equals("epics")) {
-                return Endpoint.POST_EPIC;
-            }
-        } else if (requestMethod.equals("DELETE")) {
-
-            if (pathParts[2].equals("tasks")) {
-
-                if (pathParts.length == 4) {
-                    return Endpoint.DELETE_TASK;
-                }
-            } else if (pathParts[2].equals("epics")) {
-                if (pathParts.length == 4) {
-                    return Endpoint.DELETE_EPIC;
-                }
-            } else if (pathParts[2].equals("subtasks")) {
-
-                if (pathParts.length == 4) {
-                    return Endpoint.DELETE_SUBTASK;
+        switch (requestMethod) {
+            case "GET" -> {
+                switch (pathParts[2]) {
+                    case "tasks" -> {
+                        if (pathParts.length == 3) {
+                            return Endpoint.GET_TASKS;
+                        } else {
+                            return Endpoint.GET_TASK;
+                        }
+                    }
+                    case "subtasks" -> {
+                        if (pathParts.length == 3) {
+                            return Endpoint.GET_SUBTASKS;
+                        } else {
+                            return Endpoint.GET_SUBTASK;
+                        }
+                    }
+                    case "epics" -> {
+                        if (pathParts.length == 5) {
+                            return Endpoint.GET_SUBTASK_BY_EPIC; // длина 5
+                        } else if (pathParts.length == 3) {
+                            return Endpoint.GET_EPICS; // длинна 3
+                        } else if (pathParts.length == 4) {
+                            return Endpoint.GET_EPIC; // длинна 4
+                        }
+                    }
+                    case "history" -> {
+                        return Endpoint.GET_HISTORY;
+                    }
+                    case "prioritized" -> {
+                        return Endpoint.GET_PRIORITIZED;
+                    }
                 }
             }
+            case "POST" -> {
+                switch (pathParts[2]) {
+                    case "tasks" -> {
+                        return Endpoint.POST_TASK;
+                    }
+                    case "subtasks" -> {
+                        return Endpoint.POST_SUBTASK;
+                    }
+                    case "epics" -> {
+                        return Endpoint.POST_EPIC;
+                    }
+                }
+            }
+            case "DELETE" -> {
 
+                switch (pathParts[2]) {
+                    case "tasks" -> {
+
+                        if (pathParts.length == 4) {
+                            return Endpoint.DELETE_TASK;
+                        }
+                    }
+                    case "epics" -> {
+                        if (pathParts.length == 4) {
+                            return Endpoint.DELETE_EPIC;
+                        }
+                    }
+                    case "subtasks" -> {
+
+                        if (pathParts.length == 4) {
+                            return Endpoint.DELETE_SUBTASK;
+                        }
+                    }
+                }
+            }
         }
         return Endpoint.DEFAULT;
     }
