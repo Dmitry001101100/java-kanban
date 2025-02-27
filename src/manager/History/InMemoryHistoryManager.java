4599f132f0337177d9d891f842be6bf1c22f6d11
Дмитрий.Щ <deat10poll@yandex.ru>
2025-02-27 20:56:57 +0300

    update histotyManager

--- src/manager/History/InMemoryHistoryManager.java
+++ src/manager/History/InMemoryHistoryManager.java
@@ -1,4 +1,5 @@
 package manager.History;
+
 import tasks.Task;
 
 import java.util.*;
@@ -6,11 +7,11 @@ import java.util.*;
 public class InMemoryHistoryManager implements HistoryManager {
 
     private final Map<Integer, Node<Task>> historyMap = new HashMap<>();
-
+    private Node<Task> head = null;
+    private Node<Task> tail = null;
 
     private static class Node<T> {
-
-        private  T data;
+        private T data;
         private Node<T> next;
         private Node<T> prev;
 
@@ -21,29 +22,23 @@ public class InMemoryHistoryManager implements HistoryManager {
         }
     }
 
-    private Node<Task> head;
-    private Node<Task> tail;
-
-
-
     private Node<Task> linkLast(Task task) {
+        Node<Task> newNode = new Node<>(task);
         if (head == null) {
-            head = new Node<>(task);
-            tail = head;
+            head = newNode;
+            tail = newNode;
         } else {
-            Node<Task> oldTail = tail;
-            tail = new Node<>(task);
-            tail.prev = oldTail;
-            oldTail.next = tail;
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
         }
-
         return tail;
     }
 
     private List<Task> getTasks() {
         List<Task> tasksArr = new ArrayList<>();
         Node<Task> iterator = head;
-        for (int i = 0; i < historyMap.size(); i++) {
+        while (iterator != null) {
             tasksArr.add(iterator.data);
             iterator = iterator.next;
         }
@@ -56,16 +51,15 @@ public class InMemoryHistoryManager implements HistoryManager {
         }
         if (node.prev != null) {
             node.prev.next = node.next;
-        } else { //Это head
+        } else {
             head = node.next;
         }
         if (node.next != null) {
             node.next.prev = node.prev;
-        } else { //Это tail
+        } else {
             tail = node.prev;
         }
     }
-
 
     @Override
     public void add(Task task) {
@@ -80,13 +74,16 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void remove(int id) {
-
         if (historyMap.containsKey(id)) {
             removeNode(historyMap.get(id));
             historyMap.remove(id);
         }
     }
 
-
-
+    @Override
+    public void clear() {
+        historyMap.clear();
+        head = null;
+        tail = null;
+    }
 }
